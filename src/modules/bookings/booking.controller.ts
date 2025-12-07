5b1245a28f421ab9fefe70b10cb95e37735eaca8
Iftekhar-007 <shawonsp534@gmail.com>
2025-12-07 23:39:32 +0600

    booking added

--- src/modules/bookings/booking.controller.ts
+++ src/modules/bookings/booking.controller.ts
@@ -2,21 +2,12 @@ import { Request, Response } from "express";
 import { bookingsServices } from "./booking.service";
 
 const createBookings = async (req: Request, res: Response) => {
-  const {
-    customer_id,
-    vehicle_id,
-    rent_start_date,
-    rent_end_date,
-    total_price,
-    status,
-  } = req.body;
-
   try {
     const result = await bookingsServices.createBookings(req.body);
     res.status(201).json({
       success: true,
       message: "booking created successfully",
-      data: result.rows[0],
+      data: result,
     });
   } catch (err: any) {
     res.status(504).json({
--- src/modules/bookings/booking.service.ts
+++ src/modules/bookings/booking.service.ts
@@ -1,17 +1,52 @@
 import { pool } from "../../config/db";
 
 const createBookings = async (payload: Record<string, unknown>) => {
+  const { customer_id, vehicle_id, rent_start_date, rent_end_date } = payload;
+
+  const vehicleAvailable = await pool.query(
+    `SELECT daily_rent_price, availability_status FROM vehicles WHERE id=$1`,
+    [vehicle_id]
+  );
+
+  if (vehicleAvailable.rows.length === 0) {
+    return "vehicle not found";
+  }
+
+  const vehicle = vehicleAvailable.rows[0];
+
+  if (vehicle.availability_status === "booked") {
+    return "this vehicle already booked";
+  }
+
+  const dailyPrice = Number(vehicle.daily_rent_price);
+
+  const start = new Date(payload.rent_start_date as Date);
+  const end = new Date(payload.rent_end_date as Date);
+
+  const days = Math.ceil(
+    (end.getTime() - start.getTime()) / (1000 * 60 * 60 * 24)
+  );
+
+  const total_price = days * dailyPrice;
+
   const result = await pool.query(
-    `INSERT INTO bookings(customer_id,vehicle_id,rent_start_date,rent_end_date) VALUES($1,$2,$3,$4)`,
+    `INSERT INTO bookings(customer_id,vehicle_id,rent_start_date,rent_end_date,total_price,status) VALUES($1,$2,$3,$4,$5,$6) RETURNING *`,
     [
-      payload.customer_id,
-      payload.vehicle_id,
-      payload.rent_start_date,
-      payload.rent_end_date,
+      customer_id,
+      vehicle_id,
+      rent_start_date,
+      rent_end_date,
+      total_price,
+      "active",
     ]
   );
 
-  return result;
+  await pool.query(
+    `UPDATE vehicles SET availability_status='booked' WHERE id=$1`,
+    [vehicle_id]
+  );
+
+  return result.rows[0];
 };
 
 export const bookingsServices = {
